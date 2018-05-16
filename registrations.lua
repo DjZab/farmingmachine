17e80553bb25cce69a91d27ca54afe2107b24b2d
DjZab <DjZab@users.noreply.github.com>
2018-05-16 14:50:22 +0200

    Update registrations.lua

--- registrations.lua
+++ registrations.lua
@@ -1,1 +1,104 @@
+-- The default soils
+local soil_nodenames = {
+	["farming:soil"] 	 	= true,
+	["farming:soil_wet"] 		= true,
+	["farming:desert_sand_soil"]    = true,
+	["farming:desert_sand_soil_wet"]= true
+}
 
+local seeder_seed = {
+-- *** farming
+	    {"farming:seed_barley", "farming:barley_1"},
+	    {"farming:seed_hemp", "farming:hemp_1"},
+	    {"farming:pumpkin_seed", "farming:pumpkin_1"},
+	    {"farming:coffee_beans", "farming:coffee_1"},
+	    {"farming:chili_pepper", "farming:chili_1"},
+	    {"farming:carrot", "farming:carrot_1"},
+	    {"farming:corn", "farming:corn_1"},
+	    {"farming:cucumber", "farming:cucumber_1"},
+	    {"farming:garlic_clove", "farming:garlic_1"},
+	    {"farming:melon_slice", "farming:melon_1"},
+	    {"farming:onion", "farming:onion_1"},
+	    {"farming:peppercorn", "farming:pepper_1"},
+	    {"farming:pineapple_top", "farming:pineapple_1"},
+	    {"farming:potato", "farming:potato_1"},
+	    {"farming:pumpkin_slice", "farming:pumpkin_1"},
+	    {"farming:raspberries", "farming:raspberry_1"},
+	    {"farming:rhubarb", "farming:rhubarb_1"},
+	    {"farming:tomato", "farming:tomato_1"},
+	    {"farming:blueberries", "farming:blueberry_1"},
+	    {"farming:pea_pod", "farming:pea_1"},
+	    {"farming:beetroot", "farming:beetroot_1"},
+	    
+-- *** farming_plus
+	    {"farming_plus:carrot_seed", "farming_plus:carrot_1"},
+	    {"farming_plus:chilli_seeds", "farming_plus:chilli_1"},
+	    {"farming_plus:coffee_beans", "farming_plus:coffee_1"},
+	    {"farming_plus:corn_seed", "farming_plus:corn_1"},
+	    {"farming_plus:cucumber_seed", "farming_plus:cucumber_1"},
+	    {"farming_plus:garlic_seed", "farming_plus:garlic_1"},
+	    {"farming_plus:seed_hemp", "farming_plus:hemp_1"},
+	    {"farming_plus:melon_seed", "farming_plus:melon_1"},
+	    {"farming_plus:potato_seed", "farming_plus:potato_1"},
+	    {"farming_plus:rhubarb_seed", "farming_plus:rhubarb_1"},
+	    {"farming_plus:raspberry_seed", "farming_plus:raspberry_1"},
+	    {"farming_plus:strawberry_seed", "farming_plus:strawberry_1"},
+	    {"farming_plus:soy_seed", "farming_plus:soy_plant_1"},
+	    {"farming_plus:tomato_seed", "farming_plus:tomato_1"},
+	    {"farming_plus:walnut_item", "farming_plus:walnut_1"},
+	    {"farming_plus:orange_seed", "farming_plus:orange_1"},
+	    {"farming_plus:lemon_seed", "farming_plus:lemon_1"},
+	    {"farming_plus:peach_seed", "farming_plus:peach_1"},
+	    {"farming_plus:seed_barley", "farming_plus:barley_1"},
+	    {"farming_plus:coffee_beans", "farming_plus:coffee_1"},
+	
+-- *** beer_test
+{"beer_test:seed_oats", "beer_test:seed_oats"},
+  
+  
+-- Harvesting   
+  harvester_names["farming:wheat_8"] = true 
+	harvester_names["farming:cotton_8"] = true
+	harvester_names["farming_plus:tomato"] = true
+	harvester_names["farming_plus:chilli"] = true
+	harvester_names["farming_plus:garlic"] = true
+	harvester_names["farming_plus:soy_plant"] = true
+	harvester_names["farming_plus:cucumber"] = true
+	harvester_names["farming_plus:coffee"] = true
+	harvester_names["farming_plus:potato"] = true
+	harvester_names["farming_plus:carrot"] = true
+	harvester_names["farming_plus:garlic"] = true
+	harvester_names["farming_plus:rhubarb"]= true
+	harvester_names["farming_plus:barley_7"]= true
+	harvester_names["farming_plus:hemp_8"]= true
+	harvester_names["farming_plus:beanpole_5"]= true
+	harvester_names["farming_plus:grapes_8"]= true
+	harvester_names["farming_plus:corn"]	= true
+	harvester_names["farming_plus:cornb"]	= true
+	harvester_names["farming_plus:cornc"]	= true
+	harvester_names["farming:weed"] 	= false
+	harvester_names["beer_test:oats_8"]	= true
+	harvester_names["farming_plus:melon"]	= true
+	harvester_names["farming:pumpkin"]	= true
+	harvester_names["farming:barley_7"]	= true
+	harvester_names["farming:blueberry_4"]	= true
+	harvester_names["farming:carrot_8"]	= true
+	harvester_names["farming:chili_8"]	= true
+	harvester_names["farming:coffee_5"]	= true
+	harvester_names["farming:corn_8"]	= true
+	harvester_names["farming:cucumber_4"]	= true
+	harvester_names["farming:garlic_5"]	= true
+	harvester_names["farming:hemp_8"]	= true
+	harvester_names["farming:melon_8"]	= true
+	harvester_names["farming:onion_5"]	= true
+	harvester_names["farming:pepper_5"]	= true
+	harvester_names["farming:pineapple_8"]	= true
+	harvester_names["farming:potato_4"]	= true
+	harvester_names["farming:pumpkin_8"]	= true
+	harvester_names["farming:raspberry_4"]	= true
+	harvester_names["farming:rhubarb_3"]	= true
+	harvester_names["farming:tomato_8"]	= true
+	harvester_names["farming:grapes_8"]	= true
+	harvester_names["farming:beanpole_5"]	= true
+	harvester_names["farming:pea_5"]	= true
+  harvester_names["farming:beetroot_5"] = true
