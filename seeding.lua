93e7dcaffd222b5757ff3d9be79b158eb2fd4eb6
DjZab <DjZab@users.noreply.github.com>
2018-05-16 14:47:10 +0200

    Create seeding.lua

---
+++ seeding.lua
@@ -0,0 +1,153 @@
+-- function to check for valid seeds
+local function check_valid_seed(sname)
+  
+    for i in ipairs(seeder_seed) do
+      if sname == seeder_seed[i][1] then return true end
+    end
+    return false
+end
+
+--function to give name of seedlings
+local function give_seedling(sname, util)
+
+     if not util then
+	  for i in ipairs(seeder_seed) do
+	    if sname == seeder_seed[i][1] then return seeder_seed[i][2] end
+	  end
+     else
+	  for i in ipairs(seeder_utils) do
+	    if sname == seeder_utils[i][1] then return seeder_utils[i][2] end
+	  end
+     end
+    return nil
+end
+
+--- Iterator over positions to try to saw around a sawed node.
+-- This returns positions in a 3x1x3 area around the position, plus the
+-- position above it.  This does not return the bottom position to prevent
+-- the seeder from cutting down nodes below the cutting position.
+-- @param pos Sawing position.
+function farmingNG.iterSawTries(pos)
+	-- Copy position to prevent mangling it
+	local pos = vector.new(pos)
+	local i = 0
+
+	return function()
+		i = i + 1
+		-- Given a (top view) area like so (where 5 is the starting position):
+		-- X -->
+		-- Z 123
+		-- | 456
+		-- V 789
+		-- This will return positions 1, 4, 7, 2, 8 (skip 5), 3, 6, 9,
+		-- and the position above 5.
+		if i == 1 then
+			-- Move to starting position
+			pos.x = pos.x - 1
+			pos.z = pos.z - 1
+		elseif i == 4 or i == 7 then
+			-- Move to next X and back to start of Z when we reach
+			-- the end of a Z line.
+			pos.x = pos.x + 1
+			pos.z = pos.z - 2
+		elseif i == 5 then
+			-- Skip the middle position (we've already run on it)
+			-- and double-increment the counter.
+			pos.z = pos.z + 2
+			i = i + 1
+		elseif i <= 9 then
+			-- Go to next Z.
+			pos.z = pos.z + 1
+		elseif i == 10 then
+			-- Move back to center and up.
+			-- The Y+ position must be last so that we don't dig
+			-- straight upward and not come down (since the Y-
+			-- position isn't checked).
+			pos.x = pos.x - 1
+			pos.z = pos.z - 1
+			pos.y = pos.y + 1
+		else
+			return nil
+		end
+		return pos
+	end
+end
+
+-- This function does all the hard work. Recursively we dig the node at hand
+-- if it is in the table and then search the surroundings for more stuff to dig.
+local function recursive_dig(pos, remaining_charge, seednum,seedstack, user)
+	local uppos = {x =pos.x, y =(pos.y) +1,z =pos.z}
+	local toppos = {x =pos.x, y =(pos.y) +2,z =pos.z}
+	local name = user:get_player_name()
+	
+	if remaining_charge < farmingNG.seeder_charge_per_node or seedstack:is_empty() then
+		return remaining_charge, seednum, seedstack
+	end
+	local node = minetest.get_node(pos)
+	local upper = minetest.get_node(uppos)
+	local top = minetest.get_node(toppos)
+	local seedname = seedstack:get_name()
+	local helpers = check_valid_util(upper.name)
+	
+	if node.name == "farming:weed" then
+	    remaining_charge, seednum, seedstack = recursive_dig( {x=pos.x, y=pos.y-1, z=pos.z}, remaining_charge, seednum, seedstack, user)
+	    return remaining_charge, seednum, seedstack
+	end
+
+	if not soil_nodenames[node.name] then
+		return remaining_charge, seednum, seedstack
+	end	
+	
+	if not check_valid_util(seedname) then
+	      if upper.name == "air" or upper.name == "farming:weed" then
+		    minetest.set_node(uppos, {name="air"})
+		    remaining_charge = remaining_charge - farmingNG.seeder_charge_per_node
+		    seednum = seednum +1
+		    seedstack:take_item()
+		    if remaining_charge < 1 then remaining_charge = 1 end
+		    
+		    if give_seedling(seedname,false) then
+			  minetest.add_node(uppos, {name = give_seedling(seedname,false), param2 = 1})
+			  minetest.get_node_timer(uppos):start(math.random(166, 286))
+		    end
+	      else
+		    return remaining_charge, seednum, seedstack
+	      end
+	else
+	     if (upper.name == "air" or upper.name == "farming:weed") and top.name == "air" then 
+		    minetest.set_node(uppos, {name="air"})
+		    remaining_charge = remaining_charge - farmingNG.seeder_charge_per_node
+		    seednum = seednum +1
+		    seedstack:take_item()
+		    if remaining_charge < 1 then remaining_charge = 1 end
+		    
+		    if give_seedling(seedname, true) then
+			  minetest.add_node(uppos, {name = give_seedling(seedname, true), param2 = 1})
+			  minetest.get_node_timer(uppos):start(math.random(166, 286))
+		    end
+	     else
+		    return remaining_charge, seednum, seedstack
+	     end
+	end
+
+	-- Check surroundings and run recursively if any charge left
+	for npos in farmingNG.iterSawTries(pos) do
+		if remaining_charge < farmingNG.seeder_charge_per_node then
+			break
+		end
+		if soil_nodenames[minetest.get_node(npos).name] then
+			remaining_charge, seednum, seedstack = recursive_dig(npos, remaining_charge, seednum, seedstack, user)
+		end
+	end
+	return remaining_charge, seednum, seedstack
+end
+
+
+
+-- Seeder entry point
+local function seeder_dig(pos, current_charge, seednum, seedstack, user)
+	-- Start sawing things down
+	local remaining_charge, seednum, seedstack = recursive_dig(pos, current_charge, seednum, seedstack, user)
+	minetest.sound_play("farming_nextgen_seeder", {pos = pos, gain = 1.0, max_hear_distance = 10})
+	return remaining_charge, seednum, seedstack
+end
