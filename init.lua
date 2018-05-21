bb93ac543bf0da32254345476b32466dbc7481cc
DjZab <DjZab@users.noreply.github.com>
2018-05-21 23:15:53 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
 local path = minetest.get_modpath(minetest.get_current_modname())
 
 
-dofile(path.."/aliases.lua")
-dofile(path.."/harvesting.lua")
+--dofile(path.."/aliases.lua")
+--dofile(path.."/harvesting.lua")
 dofile(path.."/machine.lua")
 dofile(path.."/registrations.lua")
 dofile(path.."/seeding.lua")
